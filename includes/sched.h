a704d60ff9149c944b8f8e750ebb90b7ee3f4f92
Mylloon <kennel.anri@tutanota.com>
2024-04-18 17:41:12 +0200

    typos

--- includes/sched.h
+++ includes/sched.h
@@ -12,11 +12,11 @@ static inline int sched_default_threads() {
 }
 
 /* Lance l'ordonnanceur
- * - nthreads : nombre de threads créer par l'ordonnanceur
+ * - nthreads : nombre de threads créer par l'ordonnanceur.
  *   Si 0, le nombre de threads sera égal au nombre de coeurs de votre machine
  *
  * - qlen : nombre minimum de tâches simultanées que l’ordonnanceur devra
- *   supporter
+ *   supporter.
  *   Retourne une erreur si l'utilisateur dépasse qlen
  *
  * - f, closure : tâche initiale
@@ -28,6 +28,6 @@ int sched_init(int nthreads, int qlen, taskfunc f, void *closure);
 /* Enfile une nouvelle tâche (f, closure) à l'ordonanceur (s)
  *
  * Peut renvoyer -1 avec errno = EAGAIN quand on dépasse la capacité de
- * l'ordonanceur.
+ * l'ordonanceur
  * */
 int sched_spawn(taskfunc f, void *closure, struct scheduler *s);
