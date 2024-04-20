9ebd5e0a2d622e43f28869d234f43c8e94d33c46
Mylloon <kennel.anri@tutanota.com>
2024-04-20 21:15:46 +0200

    Revert "random task" as it's not efficient compared to simply taking the top task
    This reverts commit 274c2d6160d6d7df86f87dc3b4167d8a79912f04.

--- src/sched.c
+++ src/sched.c
@@ -71,9 +71,6 @@ sched_init(int nthreads, int qlen, taskfunc f, void *closure)
         return -1;
     }
 
-    // Initialise l'aléatoire
-    srand(time(NULL));
-
     pthread_t threads[nthreads];
     for(int i = 0; i < nthreads; ++i) {
         if(pthread_create(&threads[i], NULL, sched_worker, &sched) != 0) {
@@ -158,13 +155,7 @@ sched_worker(void *arg)
             continue;
         }
 
-        // Extrait une tâche aléatoire de la liste
-        int random_index = rand() % (s->top + 1);
-
-        struct task_info echange = s->tasks[random_index];
-        s->tasks[random_index] = s->tasks[s->top];
-        s->tasks[s->top] = echange;
-
+        // Extrait la tâche de la pile
         taskfunc f = s->tasks[s->top].f;
         void *closure = s->tasks[s->top].closure;
         s->top--;
